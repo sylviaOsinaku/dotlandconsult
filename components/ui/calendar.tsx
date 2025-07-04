2c193fb93af76230be8cf8d8caa9ae1240a3f837
sylviaOsinaku <osinakusylvia@gmail.com>
2025-07-04 17:29:45 +0100

    i am tiredddddddddddd

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -53,10 +53,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-  IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-  IconRight: () => <ChevronRight className="h-4 w-4" />,
-}}
+//       components={{
+//   IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+//   IconRight: () => <ChevronRight className="h-4 w-4" />,
+// }}
     />
   )
 }
